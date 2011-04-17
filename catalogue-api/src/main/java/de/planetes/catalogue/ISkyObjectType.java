9874b3dbfc3d18419f1269488bbf1ba0d2302039
Thorsten Kamann <thorsten.kamann@googlemail.com>
2011-04-18 00:16:22 +0200

    task [#12380287]: Add validation annotations to the domain model
    Adding validation to ISkyObjectType

--- catalogue-api/src/main/java/de/planetes/catalogue/ISkyObjectType.java
+++ catalogue-api/src/main/java/de/planetes/catalogue/ISkyObjectType.java
@@ -1,15 +1,18 @@
 package de.planetes.catalogue;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
- * Created by IntelliJ IDEA.
- * User: kamann
- * Date: 15.04.11
- * Time: 10:06
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: kamann Date: 15.04.11 Time: 10:06 To change
+ * this template use File | Settings | File Templates.
  */
 public interface ISkyObjectType {
 
-    public String getName();
+	@NotNull
+	public String getName();
 
-    public String getCode();
+	@NotNull
+	@Size(max = 3)
+	public String getCode();
 }
